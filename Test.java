23af13b47fe821d1f53a14dc386be72766724d38
AstroByteBard <156665491+AstroByteBard@users.noreply.github.com>
2024-04-12 09:21:47 +0000

    12/4/2567

--- Test.java
+++ Test.java
@@ -1,9 +1,23 @@
-class Test {
-    public static void main (String[] arg){
-        
-        Mook Human = new Mook ("Ice", " 11 pm");
+import java.util.Scanner;
 
-        Human.sayHi();
+class Voting {
 
+    public int Choose = 0;
+    Scanner keybord = new Scanner(System.in);
+
+    private int welcome (){
+        System.out.println("Welcome Everyone This is Voting Online");
+        System.out.println("1. Voter");
+        System.out.println("2. Staff");
+        System.out.println("3. Out");
+        System.out.print("Choose...");
+        Choose = keybord.nextInt();
+        return Choose;
+    } 
+
+    public static void main(String[] arg ){
+        Voting voting = new Voting();
+        voting.welcome();
+        System.out.println(voting.Choose);
     }
 }
